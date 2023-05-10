476fe217fa1157d9531d21b5b573e101c5d2b9b5
Shaheryar Sohail <shaheryarsohailtbp@gmail.com>
2023-05-09 20:02:49 -0400

    Fix target name issue in BinaryResource.cmake

--- BinaryResource.cmake
+++ BinaryResource.cmake
@@ -3,5 +3,5 @@ function(BinaryToHeader input_file output_file target_name)
     string(LENGTH "${file_data}" file_size)
     math(EXPR file_size "${file_size} / 2") # because each byte is represented by two hex characters
     string(REGEX REPLACE "(..)" "0x\\1, " hex_data ${file_data})
-    file(WRITE ${output_file} "constexpr unsigned char _${target_name}Data[] = { ${hex_data} };constexpr size_t _${target_name}Size = sizeof(_NeuralNetworkBinaryData);")
+    file(WRITE ${output_file} "constexpr unsigned char _${target_name}Data[] = { ${hex_data} };constexpr size_t _${target_name}Size = sizeof(_${target_name}Data);")
 endfunction()
