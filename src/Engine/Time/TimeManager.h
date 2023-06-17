c613ffe7ef73ee962bff6293dcaf1341e7619ab8
Shaheryar Sohail <shaheryarsohailtbp@gmail.com>
2023-06-17 16:18:03 -0400

    Try again to implement optimal adjustments: - Piece Count - Move Count - Check
    Bench: 7847809

--- src/Engine/Time/TimeManager.h
+++ src/Engine/Time/TimeManager.h
@@ -6,6 +6,7 @@
 #ifndef STOCKDORY_TIMEMANAGER_H
 #define STOCKDORY_TIMEMANAGER_H
 
+#include "../Move/OrderedMoveList.h"
 #include "TimeControl.h"
 
 #include "../../Backend/Board.h"
@@ -34,8 +35,20 @@ namespace StockDory
             constexpr static uint8_t IncrementPartitionNumerator   = 3;
             constexpr static uint8_t IncrementPartitionDenominator = 4;
 
-            constexpr static uint8_t OptimalNumerator   = 5;
-            constexpr static uint8_t OptimalDenominator = 5;
+            constexpr static uint8_t PieceCountBase        = 32;
+            constexpr static uint8_t PieceCountNumerator   =  3;
+            constexpr static uint8_t PieceCountDenominator =  5;
+
+            constexpr static uint8_t  MoveCountBase        =  25;
+            constexpr static uint8_t  MoveCountNumerator   =   3;
+            constexpr static uint8_t  MoveCountDenominator =   4;
+            constexpr static uint64_t MoveInstantTime      = 500;
+
+            constexpr static uint8_t CheckNumerator   = 10;
+            constexpr static uint8_t CheckDenominator =  7;
+
+            constexpr static KillerTable  DummyKTable;
+            constexpr static HistoryTable DummyHTable;
 
         public:
             static TimeControl Default()
@@ -63,9 +76,57 @@ namespace StockDory
 
                 actual -= TimeOverhead;
 
-                const uint64_t optimal = actual * OptimalNumerator / OptimalDenominator;
+                uint64_t optimal = actual;
+
+                optimal = PieceCountAdjustment(board, optimal);
+                optimal =  MoveCountAdjustment(board, optimal);
+                optimal =      CheckAdjustment(board, optimal);
+
+                std::cout << "Actual: " << actual << " Optimal: " << optimal << "\n";
+
+                optimal = std::min(optimal, actual);
 
                 return TimeControl(optimal, actual);
+            }
+
+        private:
+            static uint64_t PieceCountAdjustment(const Board& board, const uint64_t optimal)
+            {
+                const uint8_t pieceCount =
+                        Count(board.PieceBoard<White>(Pawn  ) | board.PieceBoard<Black>(Pawn  )) +
+                        Count(board.PieceBoard<White>(Knight) | board.PieceBoard<Black>(Knight)) +
+                        Count(board.PieceBoard<White>(Bishop) | board.PieceBoard<Black>(Bishop)) +
+                        Count(board.PieceBoard<White>(Rook  ) | board.PieceBoard<Black>(Rook  )) +
+                        Count(board.PieceBoard<White>(Queen ) | board.PieceBoard<Black>(Queen )) +
+                        Count(board.PieceBoard<White>(King  ) | board.PieceBoard<Black>(King  ));
+
+                return std::max(optimal * pieceCount          / PieceCountBase,
+                                optimal * PieceCountNumerator / PieceCountDenominator);
+            }
+
+            static uint64_t MoveCountAdjustment(const Board& board, const uint64_t optimal)
+            {
+                uint8_t moveCount = board.ColorToMove() == White ?
+                                    OrderedMoveList<White>(
+                                            board, 0, DummyKTable, DummyHTable, NoMove
+                                    ).Count() :
+                                    OrderedMoveList<Black>(
+                                            board, 0, DummyKTable, DummyHTable, NoMove
+                                    ).Count() ;
+
+                moveCount = std::min<uint8_t>(moveCount, MoveCountBase);
+
+                return moveCount == 1 ? std::min<uint64_t>(MoveInstantTime, optimal) :
+                    std::max(optimal * moveCount          / MoveCountBase,
+                             optimal * MoveCountNumerator / MoveCountDenominator);
+            }
+
+            static uint64_t CheckAdjustment(const Board& board, const uint64_t optimal)
+            {
+                const bool checked = board.ColorToMove() == White ? board.Checked<White>() : board.Checked<Black>();
+
+                return checked ? optimal *   CheckNumerator / CheckDenominator :
+                                 optimal * CheckDenominator /   CheckNumerator ;
             }
 
     };
