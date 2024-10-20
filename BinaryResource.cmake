87a86dbc100ffcfd21139cf63c6a81f3c9da55ab
Capksz <emosker@gmail.com>
2024-10-20 00:02:04 -0400

    removed files for set up

--- BinaryResource.cmake
+++
@@ -1,7 +0,0 @@
-function(BinaryToHeader input_file output_file target_name)
-    file(READ ${input_file} file_data HEX)
-    string(LENGTH "${file_data}" file_size)
-    math(EXPR file_size "${file_size} / 2") # because each byte is represented by two hex characters
-    string(REGEX REPLACE "(..)" "0x\\1, " hex_data ${file_data})
-    file(WRITE ${output_file} "constexpr unsigned char _${target_name}Data[] = { ${hex_data} };constexpr size_t _${target_name}Size = sizeof(_${target_name}Data);")
-endfunction()
