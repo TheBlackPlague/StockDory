0d7254fba3556fb764c2d246f8d376cb1b1a0a60
Shaheryar Sohail <44123859+TheBlackPlague@users.noreply.github.com>
2025-06-07 11:26:13 -0400

    🦈 IMP: Granular Late Move Reduction/Extension Calculation
    Bench: 5969114

--- src/Engine/TunableParameter.h
+++ src/Engine/TunableParameter.h
@@ -17,7 +17,7 @@ namespace StockDory
     constexpr uint8_t TimeIncrementPartitionDenominator = 4;
     constexpr uint8_t TimeProcessingOverhead            = 10;
 
-    constexpr Array<uint16_t, 5> SearchStabilityTimeOptimizationFactor{
+    constexpr Array<uint16_t, 5> SearchStabilityTimeOptimizationFactor {
         250, 180, 120, 99, 97
     };
 
@@ -46,8 +46,11 @@ namespace StockDory
     constexpr uint8_t LMPMaximumDepth  = 3;
     constexpr uint8_t LMPLastQuietBase = 3;
 
-    constexpr uint8_t LMRMinimumDepth = 3;
-    constexpr uint8_t LMRMinimumMoves = 3;
+    constexpr  uint8_t LMRMinimumDepth      =    3;
+    constexpr  uint8_t LMRMinimumMoves      =    3;
+    constexpr uint16_t LMRNotPVBonus        = 1024;
+    constexpr uint16_t LMRNotImprovingBonus = 1024;
+    constexpr uint16_t LMRGaveCheckPenalty  = 1024;
 
     constexpr uint8_t FutilityDepthFactor = 150;
 
