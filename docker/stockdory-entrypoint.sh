7d22a540ef9728d49495165a0eb6db65733d09ee
Shaheryar Sohail <44123859+TheBlackPlague@users.noreply.github.com>
2026-09-18 16:29:28 -0500

    fix: use LLVM 22 in container entrypoint

--- docker/stockdory-entrypoint.sh
+++ docker/stockdory-entrypoint.sh
@@ -13,7 +13,7 @@ rm -rf "$BUILD_DIR"
 cmake -S "$SOURCE_DIR" -B "$BUILD_DIR" -G Ninja \
     -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_C_COMPILER=clang-20 \
-    -DCMAKE_CXX_COMPILER=clang++-20 \
+    -DCMAKE_CXX_COMPILER=clang++-22 \
     -DBUILD_NATIVE=ON \
     -DBUILD_PRODUCTION=ON
 
