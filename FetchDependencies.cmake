6ac3442bf443200d8aca13cb0c6005e54c6b72cd
Shaheryar Sohail <44123859+TheBlackPlague@users.noreply.github.com>
2026-09-26 05:27:44 -0500

    chore: update MantaRay v5
    Bench: 5894930

--- FetchDependencies.cmake
+++ FetchDependencies.cmake
@@ -3,7 +3,7 @@ include(DownloadCPM.cmake)
 CPMAddPackage(
         NAME MantaRay
         GITHUB_REPOSITORY TheBlackPlague/MantaRay
-        GIT_TAG 44ff54318a67ef44c9cb68feb0736683b1c033ed
+        GIT_TAG 6cd37f61e0ba402c1060eced94139a0f5f5a7dcf
         OPTIONS
         "BUILD_TESTS OFF"
         "BUILD_BENCHMARKS OFF"
